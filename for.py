caeb4089bfa3004ea9ce36e053b268725d7ff82c
Amanda Reges <reges.amanda1@gmail.com>
2019-11-14 21:28:52 -0300

    for.py

---
+++ for.py
@@ -0,0 +1,13 @@
+lista_nomes = ['Amanda', 'Vinicius', 'Hugo', 'Hevelin']
+
+for nome in lista_nomes:
+   print(nome)
+else:
+    print('Todos os nomes da lista foram impressos')
+
+for item in range(0,5)
+    print(item)
+    print(lista_nomes[item])
+
+for item in range(0, 11, 2)
+    print(item)
